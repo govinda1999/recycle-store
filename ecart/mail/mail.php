e7614b436e8d1f6794df29d5aa1a6b988f692f97
govinda1999 <gspatel61199@gmail.com>
2019-11-09 16:51:25 +0530

    added PHPmailer

---
+++ ecart/mail/mail.php
@@ -0,0 +1,85 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require('./vendor/autoload.php');
+// require  'PhpMailer/class.smtp.php';
+
+/*
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->Host = 'tls://smtp.gmail.com:587';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'gspatel611999@gmail.com';
+    $mail->Password   = 'Govinda@61';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    //Recipients
+    $mail->setFrom('gspatel611999@gmail.com', 'Govinda Patel');
+    $mail->addAddress('gspatel4555@gmail.com', 'Govinda Patel');
+
+
+    // Attachments
+    // Optional name
+
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Mail';
+    $mail->Body    = 'Mail send by PHPMailer';
+
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message sent!';
+    }
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+*/
+function send_mail($address, $amt, $tran)
+{
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'tls://smtp.gmail.com:587';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'gspatel611999@gmail.com';
+        $mail->Password   = 'Govinda@61';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        //Recipients
+        $mail->setFrom('gspatel611999@gmail.com', 'Govinda Patel');
+        $mail->addAddress($address);
+
+
+        // Attachments
+        // Optional name
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Thank you for using Recycle Store';
+        $mail->Body    = "Dear User,<br><br>Thank you for payment on Recycle Store.<br>Your Transaction no is <b>$tran</b> and Amount is <b>$amt</b>.";
+
+        if (!$mail->send()) {
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message sent!';
+        }
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+send_mail('gspatel4555@gmail.com', 200, 20191102020);
